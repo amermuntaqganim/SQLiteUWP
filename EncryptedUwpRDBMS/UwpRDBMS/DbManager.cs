93bc04a91ec252d900d454d82349705659495005
Amer Ganim <ganimtruthfinder@gmail.com>
2024-05-20 15:33:49 +0600

    Create Encrypted DB File

---
+++ EncryptedUwpRDBMS/UwpRDBMS/DbManager.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Input.Inking;
+using Windows.UI.ViewManagement;
+
+namespace UwpRDBMS
+{
+    public class DbManager
+    {
+
+        public static DbManager Instance = Singleton<DbManager>.Instance;
+        string dbpath;
+        SqliteConnection connection;
+        string password = "my_password";
+
+        private DbManager()
+        {
+            
+            Debug.WriteLine("Constructor called");
+            SQLitePCL.Batteries_V2.Init();
+            dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "SQLiteRDBMS.db");
+            password = "my_password";
+
+            // Create and encrypt the database
+            using (var connection = GetEncryptedConnection())
+            {
+
+                var cacheSizeCmd = connection.CreateCommand();
+                cacheSizeCmd.CommandText = "PRAGMA cache_size = 10000;";
+                cacheSizeCmd.ExecuteNonQuery();
+
+                var pageSizeCmd = connection.CreateCommand();
+                pageSizeCmd.CommandText = "PRAGMA page_size = 4096;";
+                pageSizeCmd.ExecuteNonQuery();
+            }
+        }
+
+        public SqliteConnection GetConnection()
+        {
+
+            return connection = new SqliteConnection($"Filename={dbpath}");
+
+        }
+
+        public SqliteConnection GetEncryptedConnection()
+        {
+            dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "SQLiteRDBMS.db");
+            password = "my_password";
+
+            var connectionString = new SqliteConnectionStringBuilder
+            {
+                DataSource = dbpath,
+                Password = password,
+                Mode = SqliteOpenMode.ReadWriteCreate,
+                Cache = SqliteCacheMode.Default
+            }.ToString();
+
+            var connection = new SqliteConnection(connectionString);
+            connection.Open();
+            Debug.WriteLine("Passowrd: " + password);
+
+           
+            /*var command = connection.CreateCommand();
+            command.CommandText = $"PRAGMA key = '{password}';";
+            command.ExecuteNonQuery();*/
+
+            return connection;
+        }
+
+        public void CreateTables()
+        {
+
+            //var connection = new SqliteConnection($"Filename={dbpath}");
+            var connection = GetEncryptedConnection();
+            //connection.Open();
+            
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                CREATE TABLE IF NOT EXISTS Device (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    DeviceId TEXT UNIQUE NOT NULL,
+                    DeviceName TEXT,
+                    DeviceAction TEXT
+                );
+
+                CREATE TABLE IF NOT EXISTS DeviceData (
+                    DataId INTEGER PRIMARY KEY AUTOINCREMENT,
+                    DeviceId TEXT UNIQUE NOT NULL,
+                    Data TEXT,
+                    DeviceAttribute TEXT,
+                    DeviceValue TEXT,
+                    FOREIGN KEY (DeviceId) REFERENCES Device(Id)
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE
+                );
+
+                CREATE TABLE IF NOT EXISTS DeviceSettings (
+                    SettingId INTEGER PRIMARY KEY AUTOINCREMENT,
+                    DeviceId TEXT UNIQUE NOT NULL,
+                    Setting TEXT,
+                    SettingOrder TEXT,
+                    CameraSetting TEXT,
+                    ActionSetting TEXT,
+                    FOREIGN KEY (DeviceId) REFERENCES Device(Id)
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE
+                );
+            ";
+            command.ExecuteNonQuery();
+        }
+
+    }
+}
---
+++ EncryptedUwpRDBMS/UwpRDBMS/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UwpRDBMS
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Insert_Click(object sender, RoutedEventArgs e)
+        {
+
+            DeviceDao.Instance.InsertAllDeviceInfo("device-123", "name_one", "action_one");
+            DeviceDao.Instance.InsertDeviceData("device-123", "Sample Data", "Sample data atrribute one", "sample value one");
+            DeviceDao.Instance.InsertDeviceSettings("device-123", "Sample Settings");
+
+            DeviceDao.Instance.InsertAllDeviceInfo("device-123", "name_one", "action_two");
+            DeviceDao.Instance.InsertDeviceData("device-123", "Sample Data Two", "Sample data atrribute two", "sample value two");
+            DeviceDao.Instance.InsertDeviceSettings("device-123", "Sample Settings TWO");
+
+            DeviceDao.Instance.InsertAllDeviceInfo("device-234", "name_three", "action_three");
+            DeviceDao.Instance.InsertDeviceData("device-234", "Sample Data Three", "Sample data atrribute three", "sample value three");
+            DeviceDao.Instance.InsertDeviceSettings("device-234", "Sample Settings Three");
+
+            DeviceDao.Instance.DeleteDevice("device-123");
+
+
+            List<Device> devices = DeviceDao.Instance.GetDevicesWithData();
+
+            // Example usage: Print the devices and their data
+            foreach (var device in devices)
+            {
+                Debug.WriteLine($"Device: {device.Id}, Name: {device.DeviceName}, Device Id: {device.DeviceId}");
+                foreach (var data in device.DeviceDataList)
+                {
+                    Debug.WriteLine($"\tData: {data.Data}, Additional Info: {data.DeviceAttribute}");
+                }
+                foreach (var setting in device.DeviceSettingsList)
+                {
+                    Debug.WriteLine($"\tSetting: {setting.Setting}");
+                }
+            }
+
+
+        }
+    }
+}
